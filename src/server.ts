83bcdef89aff50eaf755a5f2fa6c6233f3697bad
Robert Zavala <rbrtzavala@gmail.com>
2024-04-15 15:49:38 -0600

    install morgan for middleware; create initial routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import router from './router'
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   console.log('hello from express')
   res.status(200)
   res.json({message: 'howdy!'})
 })
+
+app.use('/api', router)
+
 export default app;
