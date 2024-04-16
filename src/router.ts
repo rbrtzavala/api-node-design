28a35d3d7157be703f3401e08cae0077f3d114d1
Robert Zavala <rbrtzavala@gmail.com>
2024-04-15 18:59:53 -0600

    start building handlers and middlewear

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express'
+import { body, oneOf } from 'express-validator'
+import { handlesInputErrors } from './modules/middleware'
 
 const router = Router()
 
@@ -7,7 +9,9 @@ const router = Router()
  */
 router.get('/product/', () => {})
 router.get('/product/:id', () => {})
-router.put('/product/:id', () => {})
+router.put('/product/:id', body('name').isString(), handlesInputErrors, (req, res) => {
+
+})
 router.post('/product', () => {})
 router.delete('/product/:id', () => {})
 
@@ -16,8 +20,17 @@ router.delete('/product/:id', () => {})
  */
 router.get('/update/', () => {})
 router.get('/update/:id', () => {})
-router.put('/update/:id', () => {})
-router.post('/update', () => {})
+router.put('/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED','DEPRECATED']),
+    body('version').optional(),
+ () => {}
+)
+router.post('/update',
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+ () => {})
 router.delete('/update/:id', () => {})
 
 /**
@@ -25,8 +38,17 @@ router.delete('/update/:id', () => {})
  */
 router.get('/updatepoint/', () => {})
 router.get('/updatepoint/:id', () => {})
-router.put('/updatepoint/:id', () => {})
-router.post('/updatepoint', () => {})
+router.put('/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => {}
+)
+router.post('/updatepoint',
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => {}
+)
 router.delete('/updatepoint/:id', () => {})
 
 export default router
