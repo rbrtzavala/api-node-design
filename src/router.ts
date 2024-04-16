8152fd8c17680901875b22e942391ad8fec6c927
Robert Zavala <rbrtzavala@gmail.com>
2024-04-16 10:54:47 -0600

    create updates handler for routes

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { body, oneOf } from 'express-validator'
 import { handlesInputErrors } from './modules/middleware'
 import { createProduct, deleteProduct, getOneProduct, getProducts } from './handlers/product'
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from './handlers/update'
 
 const router = Router()
 
@@ -27,20 +28,22 @@ router.delete('/product/:id', deleteProduct)
 /**
  * Update
  */
-router.get('/update/', () => {})
-router.get('/update/:id', () => {})
+router.get('/update/', getUpdates)
+router.get('/update/:id', getOneUpdate)
 router.put('/update/:id',
     body('title').optional(),
     body('body').optional(),
-    body('status').isIn(['IN_PROGRESS', 'SHIPPED','DEPRECATED']),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED','DEPRECATED']).optional(),
     body('version').optional(),
- () => {}
+    updateUpdate
 )
 router.post('/update',
     body('title').exists().isString(),
     body('body').exists().isString(),
- () => {})
-router.delete('/update/:id', () => {})
+    body('productId').exists().isString(),
+    createUpdate    
+)
+router.delete('/update/:id', deleteUpdate)
 
 /**
  * Update Point
